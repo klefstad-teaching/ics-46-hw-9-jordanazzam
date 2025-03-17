2720b9da58a06bc030f46caa049aae562c160bf6
jordanazzam <jelazzam@uci.edu>
2025-03-17 02:50:27 +0000

    changes

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -137,11 +137,10 @@ void print_word_ladder(const vector<string>& ladder)
     for (size_t i = 0; i < ladder.size(); ++i)
     {
         cout << ladder[i];
-        if (i < ladder.size() - 1)
+        if (i < ladder.size())
         {
             cout << " ";
         }
-        cout << " ";
     }
     cout << endl;
 }
