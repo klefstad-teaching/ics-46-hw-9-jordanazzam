74489534fbe6400934076998d5a0307e36f5655f
jordanazzam <jelazzam@uci.edu>
2025-03-17 02:20:57 +0000

    changes

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -91,7 +91,7 @@ vector<string> generate_word_ladder(const string& begin_word, const string& end_
         vector<string> ladder = ladder_queue.front();
         ladder_queue.pop();
         string last_word = ladder.back();
-        for(string &word : word_list)
+        for(const std::string &word : word_list)
         {
             if(is_adjacent(last_word, word))
             {
